3e6de832c31cf6665ee925757f7eb6a27fe3954a
localbox1994@gmail.com <localbox1994@gmail.com>
2022-03-29 22:22:31 +0530

    navbar fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -191,58 +191,7 @@
     }
   });
 
-  /**
-   * Porfolio isotope and filter
-   */
-  window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-      });
-
-      let portfolioFilters = select('#portfolio-flters li', true);
-
-      on('click', '#portfolio-flters li', function(e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
-
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        aos_init();
-      }, true);
-    }
-
-  });
-
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfokio-lightbox'
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
+ 
   /**
    * Testimonials slider
    */
@@ -276,13 +225,17 @@
   var swiper = new Swiper(".plansSwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
+    loop:false,
     breakpoints: {
       320: {
         slidesPerView: 1,
-        spaceBetween: 40
+      },
+      575:{
+        slidesPerView: 2,
       },
       768: {
-        slidesPerView: 2
+        slidesPerView: 2,
+        spaceBetween: 10,
       },
       1024: {
         slidesPerView: 3,
