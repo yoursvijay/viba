341dc326244dc6cf4246a7b57f037eab2c5cf89f
localbox1994@gmail.com <localbox1994@gmail.com>
2022-06-30 19:07:25 +0530

    added form validation changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -449,3 +449,39 @@ function closeNav() {
 $('.scheduleDemobox').click(function() {
   $('#demoOverlay').toggleClass('open');
  });
+
+
+ (function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+
+    Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('reset', function (event) {
+        if (!form.checkValidity()) {
+          // event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.remove('was-validated')
+      }, false)
+    })
+
+    // document.getElementById("resett").form.classList.remove('was-validated')
+
+})()
